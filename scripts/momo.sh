bdf94010712b79c95365b0c955535304492daa83
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-23 14:39:14 +0900

    update

--- scripts/momo.sh
+++ scripts/momo.sh
@@ -1,2 +1,3 @@
+sudo -A modprobe v4l2loopback video_nr=10,11,12,13
 cd ~/momo-2022.4.1_ubuntu-20.04_x86_64
 ./momo --video-device /dev/video10 test
--- scripts/setting.sh
+++
@@ -1,1 +0,0 @@
-sudo -A modprobe v4l2loopback video_nr=10,11,12,13
